114810e0d1e67ae671cdec800e0fc61826b9991a
liuzixu <liuzixu@xiaomi.com>
2018-01-15 23:11:49 +0800

    加了 md

--- src/js/page1.js
+++ src/js/page1.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
+import ReactMarkdown from 'react-markdown';
 
 import '../css/aside.scss';
 
-render((
-  123
-),document.querySelector('.main'));
+const mdtext = "年初并不能真的决定这一年的所有，<br>所以，且行且努力~";
+
+render(<ReactMarkdown source={mdtext} escapeHtml={false}/>,document.querySelector('.main'));
