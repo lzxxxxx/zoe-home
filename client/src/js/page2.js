e6a3f134507f3d0ea921d70a9f7fcc7e654e0677
liuzixu <liuzixu@xiaomi.com>
2018-02-12 16:29:36 +0800

    终于把列表加上了

---
+++ client/src/js/page2.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import '../css/aside.scss';
+import '../css/listitem.scss'
+
+
+class ListItem extends React.Component {
+  constructor (props){
+    super(props)
+  }
+  render (){
+    return (
+      <div className="listitem-contain">
+        <div className="title-contain">
+          <div className="listitem-title">
+            {this.props.prop.title}
+          </div>
+          <div className="listitem-subtitle">
+            <div className="time">
+              {this.props.prop.time}
+            </div>
+            <div className="tags">
+              {this.props.prop.tags}
+            </div>
+          </div>
+        </div>
+
+        <div className="content-contain">
+          {this.props.prop.content}
+        </div>
+      </div>
+    )
+  }
+}
+
+const listData = [
+  {
+    title: '博客搭建总结',
+    time: '2018年2月12日',
+    tags: '技术积累',
+    content: '这是一篇占坑博客。等真搭建完了，再让我详细说……'
+  },
+  {
+    title: '博客搭建总结',
+    time: '2018年2月12日',
+    tags: '技术积累',
+    content: '这是一篇占坑博客。等真搭建完了，再让我详细说……'
+  },
+  {
+    title: '博客搭建总结',
+    time: '2018年2月12日',
+    tags: '技术积累',
+    content: '这是一篇占坑博客。等真搭建完了，再让我详细说……'
+  },
+  {
+    title: '博客搭建总结',
+    time: '2018年2月12日',
+    tags: '技术积累',
+    content: '这是一篇占坑博客。等真搭建完了，再让我详细说……'
+  },
+]
+
+render(listData.map((item, idx)=>{
+  return (
+    <ListItem prop={item} key={idx}/>
+  )
+}) , document.querySelector('.main'))
+
+
