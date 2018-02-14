586e99b8fe0f0e1ed1c3b2dc3e89509a082f4543
liuzixu <liuzixu@xiaomi.com>
2018-02-14 15:16:55 +0800

    fasd

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const serve = require('koa-static');
 const route = require('koa-route');
-const BlogModel = require('./models/blog.js');
+const BlogModel = require('./models/blog');
 
 console.log('111111',BlogModel);
 
