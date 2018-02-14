eceb97da35c418135b6baf894af24ad065feff2c
liuzixu <liuzixu@xiaomi.com>
2018-02-14 16:32:23 +0800

    fafas

--- server/models/blog.js
+++ server/models/blog.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Blog = new Schema({
-  "time": {type:Date, default: Date.now },
-  "title":{type:String, default: "暂无标题"},
-  "desc": {type:String, default: "暂无描述"},
-  "content": {type:String, default: "暂无内容"},
+  time: {type:Date, default: Date.now },
+  title:{type:String, default: "暂无标题"},
+  desc: {type:String, default: "暂无描述"},
+  content: {type:String, default: "暂无内容"},
 })
 
 //Schema只是定义，最终要进行操作前必须用mongoose.model方法将Schema转换为Model才行
