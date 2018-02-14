95ceaa66c2a044e957d18e5721dff4e9a4e39b75
liuzixu <liuzixu@xiaomi.com>
2018-02-14 14:02:24 +0800

    修改链接

--- server/connectdb.js
+++ server/connectdb.js
@@ -3,26 +3,20 @@ const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/test';
 mongoose.connection.once('open', function() {
   console.log('open======');
-  // logger.info('MongoDB event open');
-  // logger.debug('MongoDB connected [%s]', url);
 
   mongoose.connection.on('connected', function() {
     console.log('connected======');
-    // logger.info('MongoDB event connected');
   });
 
   mongoose.connection.on('disconnected', function() {
     console.log('disconnected======');
-    // logger.warn('MongoDB event disconnected');
   });
 
   mongoose.connection.on('reconnected', function() {
     console.log('reconnected======');
-    // logger.info('MongoDB event reconnected');
   });
 
   mongoose.connection.on('error', function(err) {
-    // logger.error('MongoDB event error: ' + err);
     console.log('err====',err);
   });
 
@@ -32,7 +26,7 @@ mongoose.connection.once('open', function() {
 
 return mongoose.connect(url, {}, function(err) {
   if (err) {
-    console.log('err-----',err);
+    console.log('connect err-----',err);
     // logger.error('MongoDB connection error: ' + err);
     // return reject(err);
     process.exit(1);
