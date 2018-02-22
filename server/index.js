d2759c53c1167eb2f6b9de7bbce10f11424d29bf
liuzixu <liuzixu@xiaomi.com>
2018-02-22 18:55:30 +0800

    xiugai

--- server/index.js
+++ server/index.js
@@ -3,11 +3,10 @@ const app = new Koa();
 const fs = require('fs');
 const path = require('path');
 const serve = require('koa-static');
-const route = require('koa-route');
+const route = require('koa-router');
 const BlogModel = require('./models/blog');
 
 const mainHtml = (ctx, next) => {
-  console.log('dfsad');
   ctx.response.type = 'html';
   ctx.response.body = fs.createReadStream(path.resolve(__dirname,'../client/public/page1.html'));
 }
@@ -42,5 +41,5 @@ app.use(route.get('/',mainHtml));
 app.use(route.get('/addBlog',addBlog));
 app.use(route.get('/getBlog',getBlog));
 
-app.use(mainResource);
+app.use(mainResource);//静态文件路由，结合 public/resource/js 文件可理解
 app.listen(9999,'0.0.0.0');
