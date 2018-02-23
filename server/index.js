ed41c12e5fefa7cf44533744416a558488311f33
liuzixu <liuzixu@xiaomi.com>
2018-02-23 15:12:14 +0800

    xiugai

--- server/index.js
+++ server/index.js
@@ -7,6 +7,40 @@ const Router = require('koa-router');
 const router = new Router();
 const BlogModel = require('./models/blog');
 
+const mongoose = require('mongoose');
+//var uri = 'mongodb://username:password@hostname:port/databasename';
+const url = 'mongodb://localhost:27017/zoehome';
+mongoose.connection.once('open', function() {
+  console.log('open======!!');
+
+  mongoose.connection.on('connected', function() {
+    console.log('connected======');
+  });
+
+  mongoose.connection.on('disconnected', function() {
+    console.log('disconnected======');
+  });
+
+  mongoose.connection.on('reconnected', function() {
+    console.log('reconnected======');
+  });
+
+  mongoose.connection.on('error', function(err) {
+    console.log('err====',err);
+  });
+
+  // return resolve();
+  // return server.start();
+});
+return mongoose.connect(url, {}, function(err) {
+  if (err) {
+    console.log('connect err-----',err);
+    // logger.error('MongoDB connection error: ' + err);
+    // return reject(err);
+    process.exit(1);
+  }
+});
+
 const mainHtml = (ctx, next) => {
   ctx.response.type = 'html';
   ctx.response.body = fs.createReadStream(path.resolve(__dirname,'../client/public/page1.html'));
