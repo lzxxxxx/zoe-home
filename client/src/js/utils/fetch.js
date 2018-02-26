db6d69fe512cbc301f4aebe8a698a706e0a45f8c
liuzixu <liuzixu@xiaomi.com>
2018-02-26 17:13:49 +0800

    把 json 化的过程移到外面，返回格式化后的结果

--- client/src/js/utils/fetch.js
+++ client/src/js/utils/fetch.js
@@ -1,15 +1,16 @@
 import 'whatwg-fetch'
 
-function fetchData (url,{method = 'GET',headers={'Content-Type':'application/json'}, ...others}={}){
+async function fetchData (url,{method = 'GET',headers={'Content-Type':'application/json'}, ...others}={}){
   //method,headers,body,
   if(!(url)){
     throw(new Error('请求地址未定义'));
   }
-  return fetch(url,{//尾调用优化
+  let res = await fetch(url,{//尾调用优化
     method: method,
     headers: headers,
     ...others
-  }).then(res => res.json()).then(res => alert(JSON.stringify(res)))
+  })
+  return res.json()
 }
 
 export default fetchData;
