a0e8ada1e8568e2cae9e7ee66e6b9273ad24c656
liuzixu <liuzixu@xiaomi.com>
2018-02-26 11:04:34 +0800

    直接从浏览器请求似乎没问题

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ const addBlog = async function (ctx, next){
     content: 'content1'
   };
   let blog = new BlogModel(newBlog);
-  ctx.set('Allow','OPTIONS, GET, PUT, POST, DELETE')
+  // ctx.set('Allow','OPTIONS, GET, PUT, POST, DELETE')
   console.log(ctx);
   ctx.body = await blog.save();
 }
@@ -68,11 +68,11 @@ const mainResource = serve(path.resolve(__dirname, '../client/public/'));
 router.get('/',mainHtml);
 router.get('/addBlog',addBlog);
 router.get('/getBlog',getBlog);
-// app.use(async function(ctx,next){
-//   ctx.set('Allow','OPTIONS, GET, PUT, POST, DELETE')
-//   // console.log(ctx);
-//   await next();
-// })
+app.use(async function(ctx,next){
+  ctx.set('Allow','OPTIONS, GET, PUT, POST, DELETE')
+  // console.log(ctx);
+  await next();
+})
 app.use(router.routes()).use(router.allowedMethods());
 
 app.use(mainResource);//静态文件路由，结合 public/resource/js 文件可理解
