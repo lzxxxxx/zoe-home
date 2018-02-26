3a6455c5e517195935790896e7c52dded49bdabb
liuzixu <liuzixu@xiaomi.com>
2018-02-26 17:24:40 +0800

    yichulai

--- client/src/js/utils/fetch.js
+++ client/src/js/utils/fetch.js
@@ -1,16 +1,15 @@
 import 'whatwg-fetch'
 
-async function fetchData (url,{method = 'GET',headers={'Content-Type':'application/json'}, ...others}={}){
+function fetchData (url,{method = 'GET',headers={'Content-Type':'application/json'}, ...others}={}){
   //method,headers,body,
   if(!(url)){
     throw(new Error('请求地址未定义'));
   }
-  let res = await fetch(url,{//尾调用优化
+  return fetch(url,{//尾调用优化
     method: method,
     headers: headers,
     ...others
-  })
-  return res.json()
+  }).then(res => res.json())
 }
 
 export default fetchData;
