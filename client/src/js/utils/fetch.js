ab2da3edbcf7699cd5eef6a92c37a4c6b746261a
liuzixu <liuzixu@xiaomi.com>
2018-02-26 11:06:29 +0800

    保证除了 post 其他都可以

--- client/src/js/utils/fetch.js
+++ client/src/js/utils/fetch.js
@@ -1,6 +1,6 @@
 import 'whatwg-fetch'
 
-function fetchData (url,{method = 'POST',headers={'Content-Type':'application/json'}, ...others}={}){
+function fetchData (url,{method = 'GET',headers={'Content-Type':'application/json'}, ...others}={}){
   //method,headers,body,
   if(!(url)){
     throw(new Error('请求地址未定义'));
